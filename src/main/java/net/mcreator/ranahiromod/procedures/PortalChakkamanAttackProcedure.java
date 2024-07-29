c59949d678ba3a74294dbdf7c9d5c1b1ba974fca
Taichan-void-pia <hidetaka@DESKTOP-KFCUQ5S>
2024-07-29 12:42:28 +0900

    zombie doctor changed

--- src/main/java/net/mcreator/ranahiromod/procedures/PortalChakkamanAttackProcedure.java
+++ src/main/java/net/mcreator/ranahiromod/procedures/PortalChakkamanAttackProcedure.java
@@ -12,6 +12,10 @@ import net.minecraft.world.level.ClipContext;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.advancements.AdvancementProgress;
+import net.minecraft.advancements.Advancement;
 
 import net.mcreator.ranahiromod.init.RanahiromodModItems;
 
@@ -26,21 +30,29 @@ public class PortalChakkamanAttackProcedure {
 	public static void onRightClickEntity(PlayerInteractEvent.EntityInteract event) {
 		if (event.getHand() != event.getEntity().getUsedItemHand())
 			return;
-		execute(event, event.getLevel(), event.getEntity());
+		execute(event, event.getLevel(), event.getTarget(), event.getEntity());
 	}
 
-	public static void execute(LevelAccessor world, Entity sourceentity) {
-		execute(null, world, sourceentity);
+	public static void execute(LevelAccessor world, Entity entity, Entity sourceentity) {
+		execute(null, world, entity, sourceentity);
 	}
 
-	private static void execute(@Nullable Event event, LevelAccessor world, Entity sourceentity) {
-		if (sourceentity == null)
+	private static void execute(@Nullable Event event, LevelAccessor world, Entity entity, Entity sourceentity) {
+		if (entity == null || sourceentity == null)
 			return;
 		double xPos = 0;
 		double yPos = 0;
 		double zPos = 0;
 		double dis = 0;
 		if ((sourceentity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY).getItem() == RanahiromodModItems.WAWAWAWAWAWAWAWAWAWAWAWAWAWAWAWAWAWAWAWAWAWAWAWAWAWAWAWAWAWA.get()) {
+			if (entity instanceof ServerPlayer _player) {
+				Advancement _adv = _player.server.getAdvancements().getAdvancement(new ResourceLocation("ranahiromod:zombie_doctor"));
+				AdvancementProgress _ap = _player.getAdvancements().getOrStartProgress(_adv);
+				if (!_ap.isDone()) {
+					for (String criteria : _ap.getRemainingCriteria())
+						_player.getAdvancements().award(_adv, criteria);
+				}
+			}
 			dis = 0;
 			for (int index0 = 0; index0 < 5; index0++) {
 				xPos = sourceentity.level().clip(new ClipContext(sourceentity.getEyePosition(1f), sourceentity.getEyePosition(1f).add(sourceentity.getViewVector(1f).scale(dis)), ClipContext.Block.OUTLINE, ClipContext.Fluid.NONE, sourceentity))
